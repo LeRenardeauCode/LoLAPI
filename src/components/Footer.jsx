9322cbaef4ee428dd13ad7480adda7bbab51d8e4
= <=>
2025-11-17 18:21:01 +0100

    fix: footer rendu fix en bas des pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,19 @@ const Footer = () => {
     <Box
       component="footer"
       sx={{
+        position: 'fixed',
+        bottom: 0,                
+        left: 0,                  
+        width: '100%',
         py: 3,
         px: 2,
         mt: 'auto',
         backgroundColor: (theme) =>
-          theme.palette.mode === 'light'
+          theme.palette.mode === 'dark'
             ? theme.palette.grey[200]
             : theme.palette.grey[800],
         textAlign: 'center',
+        zIndex: 1000
       }}
     >
       <Typography variant="body2" color="text.secondary">
